97bb17fd3b7c7c9f38272f2b6af76c4ae66a988f
endriob <endriobiasi@gmail.com>
2019-06-18 21:55:23 -0300

    mudança da flag de input_file (antes -f ; agora -i)

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import argparse, time, re
 from leaks import Leaks
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--input_file", required=True, help="Path to the emails file")
+ap.add_argument("-i", "--input_file", required=True, help="Path to the emails file")
 ap.add_argument("-o", "--output_file", required=True, help="Output file with the leaks")
 args = vars(ap.parse_args())
 
 if __name__ == '__main__':
     Leaks(args["input_file"], args["output_file"]).run()
-    os.system("rm ghostdriver.log")
+    # os.system("rm ghostdriver.log")
 
